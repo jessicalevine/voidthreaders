c8fa525a6c910bca2f86222d905b5ac2f9def4a0
Jess Levine <jessica.s.levine@gmail.com>
2023-09-22 07:54:12 -0400

    Remove extraneous methods and comments

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
     [SerializeField] private float speed;
 
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
-    // Update is called once per frame
     void Update() {
         if (Input.GetKey(KeyCode.W)) {
             transform.position = transform.position + Vector3.up * speed;
